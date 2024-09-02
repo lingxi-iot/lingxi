710b8854daedd67d8b035f01362e7224f1d0a901
凡尔赛人 <78980809@qq.com>
2024-09-02 17:46:08 +0800

    create module project

---
+++ dashbord/src/test/java/cn/gofree/lingxi/dashbord/DashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gofree.lingxi.dashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashbordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
